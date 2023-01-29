638d072d0b425d3951dd5bf581b1f0866d7bbe86
ZY1YOGI <ZY1YOGI@GMAIL.COM>
2023-01-29 10:49:23 +0200

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.get("/", (req, res) => {
+    res.render("home")
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ public/js/list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "Harley Bird - Home",
+        artist: "Jordan Scho",
+        img: "1.jpg",
+        src: "music-1",
+    },
+    {
+        name: "Ikson Anywhere – Ikson",
+        artist: "Audio Library",
+        img: "2.jpg",
+        src: "music-2",
+    },
+    {
+        name: "Beauz & Jvna - Crazy",
+        artist: "Beauz & Jvna",
+        img: "3.jpg",
+        src: "music-3",
+    },
+    {
+        name: "Hardwind - Want Me",
+        artist: "Mike Archangelo",
+        img: "4.jpg",
+        src: "music-4",
+    },
+    {
+        name: "Jim - Sun Goes Down",
+        artist: "Jim Yosef x Roy",
+        img: "5.png",
+        src: "music-5",
+    },
+    {
+        name: "Lost Sky - Vision NCS",
+        artist: "NCS Release",
+        img: "6.jpg",
+        src: "music-6",
+    },
+];
